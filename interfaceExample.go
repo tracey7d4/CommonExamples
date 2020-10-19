ac6aa88048812bcaa566b6c74d3a40c3c61bc698
tracey <65693735+Tracey7d4@users.noreply.github.com>
2020-10-19 16:00:17 +1100

    interfaceExample

--- interfaceExample.go
+++ interfaceExample.go
@@ -3,114 +3,98 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"strconv"
 	"strings"
 )
 
-type Animal interface {
-	Eat()
-	Move()
-	Speak()
+type geometry interface {
+	area() float64
+	perim() float64
 }
 
-type Cow string
-
-func (animal Cow) Eat() {
-	fmt.Println("grass")
-}
-func (animal Cow) Move() {
-	fmt.Println("walk")
-}
-func (animal Cow) Speak() {
-	fmt.Println("moo")
+type circle struct {
+	radius float64
 }
 
-type Bird string
-
-func (animal Bird) Eat() {
-	fmt.Println("worms")
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
 }
-func (animal Bird) Move() {
-	fmt.Println("fly")
-}
-func (animal Bird) Speak() {
-	fmt.Println("peep")
+func (c circle) perim() float64 {
+	return math.Pi * 2 * c.radius
 }
 
-type Snake string
-
-func (animal Snake) Eat() {
-	fmt.Println("mice")
-}
-func (animal Snake) Move() {
-	fmt.Println("slither")
-}
-func (animal Snake) Speak() {
-	fmt.Println("hsss")
+type rectangle struct {
+	x, y float64
 }
 
-func queryRespond(animal Animal, i string) {
-	switch s := animal.(type) {
-	case Cow:
-		switch i {
-		case "eat", "Eat":
-			s.Eat()
-		case "speak", "Speak":
-			s.Speak()
-		case "move", "Move":
-			s.Move()
-		}
-	case Bird:
-		switch i {
-		case "eat", "Eat":
-			s.Eat()
-		case "speak", "Speak":
-			s.Speak()
-		case "move", "Move":
-			s.Move()
-		}
-	case Snake:
-		switch i {
-		case "eat", "Eat":
-			s.Eat()
-		case "speak", "Speak":
-			s.Speak()
-		case "move", "Move":
-			s.Move()
-		}
+func (r rectangle) area() float64 {
+	return r.x * r.y
+}
+func (r rectangle) perim() float64 {
+	return 2 * (r.x + r.y)
+}
+
+type triangle struct {
+	a, b, c float64
+}
+
+func (t triangle) area() float64 {
+	return t.a + t.b + t.c
+}
+func (t triangle) perim() float64 {
+	s := t.area()
+	return math.Sqrt(s * (s - t.a) * (s - t.b) * (s - t.c))
+}
+
+func measure(g geometry, i string) {
+	switch shape := g.(type) {
+	case circle:
+		shape.area()
+		shape.perim()
+	case rectangle:
+		shape.area()
+		shape.perim()
+	case triangle:
+		shape.area()
+		shape.perim()
 	}
-
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	m := make(map[string]Animal)
-	var animal Animal
+	var g geometry
 	fmt.Printf("> ")
 	for scanner.Scan() {
 		data := scanner.Text()
-		d := strings.Split(data, " ")
-		switch d[0] {
-		// In case newanimal, d :=["newanimal", name, type]
-		case "newanimal":
-			switch d[2] {
-			case "cow", "Cow":
-				animal = Cow(d[1])
-			case "bird", "Bird":
-				animal = Bird(d[1])
-			case "snake", "Snake":
-				animal = Snake(d[1])
-			}
-			m[d[1]] = animal
-			fmt.Println("Created it!")
-		// In case query, d :=["query", name, information requested, i.e eat, move, speak]
-		case "query":
-			if _, ok := m[d[1]]; ok {
-				queryRespond(m[d[1]], d[2])
-			} else {
-				fmt.Println("Given animal doesn't exist")
-			}
+		switch data {
+		case "circle", "Circle":
+			fmt.Printf("Radius = ")
+			scanner.Scan()
+			d := scanner.Text()
+			r, _ := strconv.ParseFloat(d, 64)
+			g = circle{r}
+		case "rectangle", "Rectangle":
+			fmt.Printf("length and width = ")
+			scanner.Scan()
+			d := scanner.Text()
+			slice := strings.Split(d, " ")
+			l, _ := strconv.ParseFloat(slice[0], 64)
+			w, _ := strconv.ParseFloat(slice[1], 64)
+			g = rectangle{l, w}
+		case "triangle", "Triangle":
+			fmt.Printf("sides = ")
+			scanner.Scan()
+			d := scanner.Text()
+			slice := strings.Split(d, " ")
+			a, _ := strconv.ParseFloat(slice[0], 64)
+			b, _ := strconv.ParseFloat(slice[1], 64)
+			c, _ := strconv.ParseFloat(slice[2], 64)
+			g = triangle{a, b, c}
 		}
+		fmt.Printf("Area      : %0.3f\n", g.area())
+		fmt.Printf("Perimeter : %0.3f\n", g.perim())
 		fmt.Printf("> ")
 	}
 }
