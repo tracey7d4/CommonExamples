1b0941bcf5732fe2cf20dd1a311479e7edca6401
tracey <65693735+Tracey7d4@users.noreply.github.com>
2020-10-20 14:10:12 +1100

    merge sort

--- sort/mergeSort.go
+++ sort/mergeSort.go
@@ -4,58 +4,38 @@ import "fmt"
 
 // mergeSort O(nlgn)
 
-func MergeSort(items []int64) []int64 {
-	var num = len(items)
-
-	if num == 1 {
-		return items
+func mergeSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
 	}
-
-	middle := int(num / 2)
-	var (
-		left  = make([]int64, middle)
-		right = make([]int64, num-middle)
-	)
-	for i := 0; i < num; i++ {
-		if i < middle {
-			left[i] = items[i]
-		} else {
-			right[i-middle] = items[i]
-		}
-	}
-
-	return merge(MergeSort(left), MergeSort(right))
+	mid := len(arr) / 2
+	left := arr[:mid]
+	right := arr[mid:]
+	return merge(mergeSort(left), mergeSort(right))
 }
 
-func merge(left, right []int64) (result []int64) {
-	result = make([]int64, len(left)+len(right))
-
-	i := 0
-	for len(left) > 0 && len(right) > 0 {
+func merge(left, right []int) []int {
+	res := make([]int,0, len(left)+len(right))
+	for len(left) > 0 || len(right) > 0 {
+		if len(left) == 0 {
+			return append(res, right...)
+		}
+		if len(right) == 0 {
+			return append(res, left...)
+		}
 		if left[0] < right[0] {
-			result[i] = left[0]
+			res = append(res, left[0])
 			left = left[1:]
 		} else {
-			result[i] = right[0]
+			res = append(res, right[0])
 			right = right[1:]
 		}
-		i++
 	}
-
-	for j := 0; j < len(left); j++ {
-		result[i] = left[j]
-		i++
-	}
-	for j := 0; j < len(right); j++ {
-		result[i] = right[j]
-		i++
-	}
-
-	return
+	return res
 }
 
 func main() {
-	arr := []int64{2, 3, 5, 6, 4, 8}
+	arr := []int{2, 3, 5, 6, 4, 8}
 
-	fmt.Println(MergeSort(arr))
+	fmt.Println(mergeSort(arr))
 }
