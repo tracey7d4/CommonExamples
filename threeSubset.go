7269006368d3caa09b97c017a0cfa05891126923
tracey <65693735+Tracey7d4@users.noreply.github.com>
2020-11-11 15:22:16 +1100

    threeSubset

--- threeSubset.go
+++ threeSubset.go
@@ -11,50 +11,50 @@ func main() {
 	fmt.Println(partitionArray(arr))
 }
 
-func partitionArray(arr []int) int {
+func partitionArray(arr []int) bool {
 	n := len(arr)
 	sum := 0
 	for _, v := range arr {
 		sum += v
 	}
 	if n < 3 || sum%3 != 0 {
-		return 0
+		return false
 	}
 	k := sum / 3
 
-	m := make(map[string]int)
+	m := make(map[string]bool)
 	return threeSubset(arr, k, k, k, m)
 }
 
-func threeSubset(arr []int, k1, k2, k3 int, m map[string]int) int {
+func threeSubset(arr []int, k1, k2, k3 int, m map[string]bool) bool {
 	n := len(arr)
 	if k1 == 0 && k2 == 0 && k3 == 0 {
-		return 1
+		return true
 	}
 	if n == 0 {
-		return 0
+		return false
 	}
 	s := strconv.Itoa(k1) + "+" + strconv.Itoa(k2) + "+" + strconv.Itoa(k3) + "+" + strconv.Itoa(n)
 	if _,ok := m[s]; ok {
 		return m[s]
 	}
-	a := 0
+	a := false
 	if k1 >= arr[0] {
 		a = threeSubset(arr[1:], k1-arr[0], k2, k3, m)
 	}
-	if a == 1 {
-		m[s] = 1
+	if a == true {
+		m[s] = true
 		return a
 	}
-	b := 0
+	b := false
 	if k2 >= arr[0] {
 		b = threeSubset(arr[1:], k1, k2-arr[0], k3, m)
 	}
-	if b == 1 {
-		m[s] = 1
+	if b == true {
+		m[s] = true
 		return b
 	}
-	c := 0
+	c := false
 	if k2 >= arr[0] {
 		c = threeSubset(arr[1:], k1, k2, k3-arr[0], m)
 	}
